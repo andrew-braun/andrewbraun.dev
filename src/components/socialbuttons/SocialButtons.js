8508145259b06faea90cb00f7c57dca140ba293b
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-08-25 00:04:02 +0400

    fixed index.js socialbuttons import

--- src/components/socialbuttons/SocialButtons.js
+++ src/components/socialbuttons/SocialButtons.js
@@ -1,6 +1,12 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { SiCodepen as CodePenIcon, SiGithub as GitHubIcon, SiFacebook as FacebookIcon, SiTwitter as TwitterIcon, SiLinkedin as LinkedInIcon } from "react-icons/si"
+import {
+	SiCodepen as CodePenIcon,
+	SiGithub as GitHubIcon,
+	SiFacebook as FacebookIcon,
+	SiTwitter as TwitterIcon,
+	SiLinkedin as LinkedInIcon,
+} from "react-icons/si"
 import styles from "./socialbuttons.module.css"
 
 export default function SocialButtons() {
@@ -39,31 +45,33 @@ export default function SocialButtons() {
 	const socialElements = socialArray.map(item => {
 		return (
 			<div className={styles.socialButton} key={`${item.name}-button`}>
-				<a href={item.link} alt={item.name}
-				className={styles.socialLink}>
-					{
-						(item.name === "Facebook") ? <span className={styles.facebookIcon}> <FacebookIcon /></span> :
-						(item.name === "GitHub") ? <span className={styles.githubIcon}><GitHubIcon /></span> :
-						(item.name === "CodePen") ? <span className={styles.codepenIcon}><CodePenIcon /></span> :
-						(item.name === "Twitter") ? <span className={styles.twitterIcon}><TwitterIcon /></span> :
-						(item.name === "LinkedIn") ? <span className={styles.linkedinIcon}><LinkedInIcon /></span> : null
-					}
+				<a href={item.link} alt={item.name} className={styles.socialLink}>
+					{item.name === "Facebook" ? (
+						<span className={styles.facebookIcon}>
+							{" "}
+							<FacebookIcon />
+						</span>
+					) : item.name === "GitHub" ? (
+						<span className={styles.githubIcon}>
+							<GitHubIcon />
+						</span>
+					) : item.name === "CodePen" ? (
+						<span className={styles.codepenIcon}>
+							<CodePenIcon />
+						</span>
+					) : item.name === "Twitter" ? (
+						<span className={styles.twitterIcon}>
+							<TwitterIcon />
+						</span>
+					) : item.name === "LinkedIn" ? (
+						<span className={styles.linkedinIcon}>
+							<LinkedInIcon />
+						</span>
+					) : null}
 				</a>
 			</div>
 		)
 	})
 
-	// const {
-	// 	facebook,
-	// 	codepen,
-	// 	github,
-	// 	linkedin,
-	// 	twitter,
-	// } = data.allSocialInfoJson.edges[0].node
-
-	return (
-		<div className={styles.socialButtons}>
-			{socialElements}
-		</div>
-	)
+	return (<div className={styles.socialButtons}>{socialElements}</div>)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,16 @@ import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/hero/Hero"
 import styles from "./index.module.css"
-import SocialButtons from "../components/socialButtons/socialButtons"
+import SocialButtons from "../components/socialbuttons/SocialButtons"
 
 export default function Home() {
 	return (
 		<Layout>
 			<main className={styles.homeMain}>
 				<div className={styles.homeEntry}>
-          <div className={styles.heroContainer}>
-					  <Hero />
-          </div>
+					<div className={styles.heroContainer}>
+						<Hero />
+					</div>
 					<div className={styles.socialContainer}>
 						<SocialButtons />
 					</div>
