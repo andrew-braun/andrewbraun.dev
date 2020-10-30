2757a3d23c9b7d37abe8b436ec118a79624ad0b0
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-10-30 12:46:24 +0400

    style: Remove logo/nav temporarily

--- src/components/layout/logo/Logo.js
+++ src/components/layout/logo/Logo.js
@@ -1,18 +1,17 @@
-import React from "react";
+import React from "react"
 import { Link } from "gatsby"
 import styles from "./logo.module.css"
 import logoImg from "./logo.svg"
 
-export default function Logo( ) {
-
-    return(
-        <div className={styles.logoContainer}>
-            <Link to="/">
-            <img 
+export default function Logo() {
+	return (
+		<div className={styles.logoContainer}>
+			<Link to="/">
+				{/* <img 
                 className={styles.logoImg}
                 src={logoImg} 
-                alt="Andrew Braun logo"/>
-            </Link>
-        </div>
-    )
+                alt="Andrew Braun logo"/> */}
+			</Link>
+		</div>
+	)
 }
--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -4,15 +4,15 @@ import "../../../styles/global.css"
 import styles from "./nav.module.css"
 
 export default function Nav() {
-    return (
-        <nav className={styles.mainNav}>
-            <ul className={styles.mainNavList}>
-                <li className={styles.mainNavItem}>
-                    <Link to="/" className={styles.mainNavLink}>
+	return (
+		<nav className={styles.mainNav}>
+			<ul className={styles.mainNavList}>
+				<li className={styles.mainNavItem}>
+					{/* <Link to="/" className={styles.mainNavLink}>
                         Home
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    )
+                    </Link> */}
+				</li>
+			</ul>
+		</nav>
+	)
 }
