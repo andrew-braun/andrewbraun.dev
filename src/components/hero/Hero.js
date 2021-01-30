0cf09d5a93ccbdf09fe5c0def6de2ac1c9f068fe
Andrew Braun <andrew@andrewbraun.dev>
2021-01-30 15:26:39 +0400

    refactor: Separate hero image into InfoImage component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../infoimage/InfoImage"
 import "../../styles/global.css"
 import styles from "./hero.module.css"
 
---
+++ src/components/infoimage/InfoImage.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../../styles/global.css"
+import styles from "./infoimage.module.css"
+
+export default function Hero() {
+	return (
+		<div className={styles.infoImageContainer}>
+			<div className={styles.background}></div>
+		</div>
+	)
+}
