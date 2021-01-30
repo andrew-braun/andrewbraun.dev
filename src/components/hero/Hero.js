fa12bf6a8184b37ca3e8b44eda9fd7ab89894717
Andrew Braun <andrew@andrewbraun.dev>
2021-01-30 17:02:27 +0400

    feat: Add icons to InfoImage

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,6 +1,26 @@
 import React from "react"
 import InfoImage from "../infoimage/InfoImage"
-import { DiJavascript1 } from "react-icons/di"
+import {
+	DiJavascript1,
+	DiReact,
+	DiWordpress,
+	DiHtml5,
+	DiCss3,
+	DiPython,
+	DiPostgresql,
+	DiGit,
+	DiPhotoshop,
+	DiVisualstudio,
+	DiPhp,
+} from "react-icons/di"
+import { GrGatsbyjs, GrGraphQl } from "react-icons/gr"
+import {
+	SiNextDotJs,
+	SiWoocommerce,
+	SiRaspberrypi,
+	SiFigma,
+	SiWebpack,
+} from "react-icons/si"
 import "../../styles/global.css"
 import styles from "./hero.module.css"
 
@@ -22,13 +42,51 @@ export default function Hero() {
 				<InfoImage
 					icon1={<DiJavascript1 />}
 					icon1Color="hsla(52, 84%, 63%, 1)"
-					icon2={<DiJavascript1 />}
-					icon3={<DiJavascript1 />}
-					icon4={<DiJavascript1 />}
-					icon5={<DiJavascript1 />}
-					icon6={<DiJavascript1 />}
-					icon7={<DiJavascript1 />}
-					icon8={<DiJavascript1 />}
+					icon1Alt="JavaScript"
+					icon2={<DiReact />}
+					icon2Color="hsla(52, 84%, 63%, 1)"
+					icon2Alt="JavaScript"
+					icon3={<DiWordpress />}
+					icon3Color="hsla(52, 84%, 63%, 1)"
+					icon4={<DiHtml5 />}
+					icon4Color="hsla(52, 84%, 63%, 1)"
+					icon4Alt="JavaScript"
+					icon5={<DiCss3 />}
+					icon5Color="hsla(52, 84%, 63%, 1)"
+					icon5Alt="JavaScript"
+					icon6={<GrGatsbyjs />}
+					icon6Color="hsla(52, 84%, 63%, 1)"
+					icon6Alt="JavaScript"
+					icon7={<DiPython />}
+					icon7Color="hsla(52, 84%, 63%, 1)"
+					icon7Alt="JavaScript"
+					icon8={<DiPostgresql />}
+					icon8Color="hsla(52, 84%, 63%, 1)"
+					icon8Alt="JavaScript"
+					icon9={<DiGit />}
+					icon9Color="hsla(52, 94%, 63%, 1)"
+					icon9Alt="JavaScript"
+					icon10={<DiPhotoshop />}
+					icon10Color="hsla(52, 104%, 63%, 1)"
+					icon10Alt="JavaScript"
+					icon11={<SiFigma />}
+					icon11Color="hsla(52, 84%, 63%, 1)"
+					icon11Alt="JavaScript"
+					icon12={<DiVisualstudio />}
+					icon12Color="hsla(52, 84%, 63%, 1)"
+					icon12Alt="JavaScript"
+					icon13={<DiPhp />}
+					icon13Color="hsla(52, 84%, 63%, 1)"
+					icon13Alt="JavaScript"
+					icon14={<SiWebpack />}
+					icon14Color="hsla(52, 84%, 63%, 1)"
+					icon14Alt="JavaScript"
+					icon15={<SiWoocommerce />}
+					icon15Color="hsla(52, 84%, 63%, 1)"
+					icon15Alt="JavaScript"
+					icon16={<SiRaspberrypi />}
+					icon16Color="hsla(52, 84%, 63%, 1)"
+					icon16Alt="JavaScript"
 				/>
 			</div>
 		</div>
--- src/components/infoimage/InfoImage.js
+++ src/components/infoimage/InfoImage.js
@@ -3,7 +3,24 @@ import "../../styles/global.css"
 import styles from "./infoimage.module.css"
 
 export default function Hero(props) {
-	const { icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8 } = props
+	const {
+		icon1,
+		icon2,
+		icon3,
+		icon4,
+		icon5,
+		icon6,
+		icon7,
+		icon8,
+		icon9,
+		icon10,
+		icon11,
+		icon12,
+		icon13,
+		icon14,
+		icon15,
+		icon16,
+	} = props
 	const {
 		icon1Color,
 		icon2Color,
@@ -13,21 +30,150 @@ export default function Hero(props) {
 		icon6Color,
 		icon7Color,
 		icon8Color,
+		icon9Color,
+		icon10Color,
+		icon11Color,
+		icon12Color,
+		icon13Color,
+		icon14Color,
+		icon15Color,
+		icon16Color,
+	} = props
+
+	const {
+		icon1Alt,
+		icon2Alt,
+		icon3Alt,
+		icon4Alt,
+		icon5Alt,
+		icon6Alt,
+		icon7Alt,
+		icon8Alt,
+		icon9Alt,
+		icon10Alt,
+		icon11Alt,
+		icon12Alt,
+		icon13Alt,
+		icon14Alt,
+		icon15Alt,
+		icon16Alt,
 	} = props
 	return (
 		<div className={styles.infoImageContainer}>
 			<div className={styles.background}>
 				<div className={styles.iconContainer}>
-					<div className={styles.icon} style={{ color: `${icon1Color}` }}>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
 						{icon1}
 					</div>
-					<div className={styles.icon}>{icon1}</div>
-					<div className={styles.icon}>{icon1}</div>
-					<div className={styles.icon}>{icon1}</div>
-					<div className={styles.icon}>{icon1}</div>
-					<div className={styles.icon}>{icon1}</div>
-					<div className={styles.icon}>{icon1}</div>
-					<div className={styles.icon}>{icon1}</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon2Color}` }}
+						title={icon2Alt}
+					>
+						{icon2}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon3}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon4}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon5}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon6}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon7}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon8}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon9}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon10}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon11}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon12}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon13}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon14}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon15}
+					</div>
+					<div
+						className={styles.icon}
+						style={{ color: `${icon1Color}` }}
+						title={icon1Alt}
+					>
+						{icon16}
+					</div>
 				</div>
 			</div>
 		</div>
