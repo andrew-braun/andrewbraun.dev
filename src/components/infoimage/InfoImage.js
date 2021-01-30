50ff27184232f6681e4e4107229f5deaec147a5f
Andrew Braun <andrew@andrewbraun.dev>
2021-01-30 17:24:37 +0400

    style: Add brand colors for icons

--- src/components/infoimage/InfoImage.js
+++ src/components/infoimage/InfoImage.js
@@ -78,99 +78,99 @@ export default function Hero(props) {
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon3Color}` }}
+						title={icon3Alt}
 					>
 						{icon3}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon4Color}` }}
+						title={icon4Alt}
 					>
 						{icon4}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon5Color}` }}
+						title={icon5Alt}
 					>
 						{icon5}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon6Color}` }}
+						title={icon6Alt}
 					>
 						{icon6}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon7Color}` }}
+						title={icon7Alt}
 					>
 						{icon7}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon8Color}` }}
+						title={icon8Alt}
 					>
 						{icon8}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon9Color}` }}
+						title={icon9Alt}
 					>
 						{icon9}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon10Color}` }}
+						title={icon10Alt}
 					>
 						{icon10}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon11Color}` }}
+						title={icon11Alt}
 					>
 						{icon11}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon12Color}` }}
+						title={icon12Alt}
 					>
 						{icon12}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon13Color}` }}
+						title={icon13Alt}
 					>
 						{icon13}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon14Color}` }}
+						title={icon14Alt}
 					>
 						{icon14}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon15Color}` }}
+						title={icon15Alt}
 					>
 						{icon15}
 					</div>
 					<div
 						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
+						style={{ color: `${icon16Color}` }}
+						title={icon16Alt}
 					>
 						{icon16}
 					</div>
