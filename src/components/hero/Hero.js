1ed87c037210a1a70309c39534d378a74228b8f8
Andrew Braun <andrew@andrewbraun.dev>
2021-01-31 13:58:21 +0400

    refactor: Programmatically create icons rather than manually placing

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -25,6 +25,98 @@ import "../../styles/global.css"
 
 import styles from "./hero.module.css"
 
+const iconList = [
+	{
+		icon: <DiJavascript1 />,
+		color: "hsla(52, 84%, 63%, 1)",
+		alt: "JavaScript",
+	},
+	{
+		icon: <DiReact />,
+		color: "hsla(192, 95%, 68%, 1)",
+		alt: "React",
+	},
+	{
+		icon: <DiCss3 />,
+		color: "hsla(207, 100%, 42%, 1)",
+		alt: "CSS3",
+	},
+	{
+		icon: <DiHtml5 />,
+		color: "hsla(18, 88%, 55%, 1)",
+		alt: "HTML5",
+	},
+	{
+		icon: <DiWordpress />,
+		color: "hsla(192, 95%, 68%, 1)",
+		alt: "WordPress",
+	},
+	{
+		icon: <GrGatsbyjs />,
+		color: "hsla(0, 0%, 100%, 0.8)",
+		alt: "Gatsby",
+	},
+	{
+		icon: <DiPython />,
+		color: "hsla(192, 95%, 68%, 1)",
+		alt: "Python",
+	},
+	{
+		icon: <DiPostgresql />,
+		color: "hsla(192, 95%, 68%, 1)",
+		alt: "PostgreSQL",
+	},
+	{
+		icon: <DiGit />,
+		color: "hsla(0, 0%, 100%, 0.8)",
+		alt: "Git",
+	},
+	{
+		icon: <DiPhotoshop />,
+		color: "hsla(210, 95%, 77%, 1)",
+		alt: "Photoshop",
+	},
+
+	{
+		icon: <SiFigma />,
+		color: "hsla(18, 88%, 55%, 1)",
+		alt: "Figma",
+	},
+
+	{
+		icon: <DiVisualstudio />,
+		color: "hsla(207, 100%, 42%, 1)",
+		alt: "VSCode",
+	},
+
+	{
+		icon: <DiPhp />,
+		color: "hsla(207, 100%, 42%, 1)",
+		alt: "PHP",
+	},
+
+	{
+		icon: <SiWebpack />,
+		color: "hsla(207, 100%, 42%, 1)",
+		alt: "Webpack",
+	},
+	{
+		icon: <SiWoocommerce />,
+		color: "hsla(312, 26%, 47%, 1)",
+		alt: "WooCommerce",
+	},
+
+	{
+		icon: <SiRaspberrypi />,
+		color: "hsla(312, 26%, 47%, 1)",
+		alt: "RaspberryPi",
+	},
+]
+
+// iconList.map(icon => {
+// 	console.log(`console-iconListMap ${icon.color}`)
+// })
+
 export default function Hero() {
 	return (
 		<div className={styles.hero}>
@@ -40,56 +132,7 @@ export default function Hero() {
 				</span>
 			</div>
 			<div className={styles.heroImageContainer}>
-				<InfoImage
-					icon1={<DiJavascript1 />}
-					icon1Color="hsla(52, 84%, 63%, 1)"
-					icon1Alt="JavaScript"
-					icon2={<DiReact />}
-					icon2Color="hsla(192, 95%, 68%, 1)"
-					icon2Alt="React"
-					icon3={<DiWordpress />}
-					icon3Color="hsla(195, 100%, 31%, 1)"
-					icon3Alt="WordPress"
-					icon4={<DiHtml5 />}
-					icon4Color="hsla(18, 88%, 55%, 1)"
-					icon4Alt="HTML5"
-					icon5={<DiCss3 />}
-					icon5Color="hsla(222, 88%, 55%, 1)"
-					icon5Alt="CSS3"
-					icon6={<GrGatsbyjs />}
-					icon6Color="hsla(270, 50%, 40%, 1)"
-					icon6Alt="Gatsby"
-					icon7={<DiPython />}
-					icon7Color="hsla(207, 45%, 49%, 1)"
-					icon7Alt="Python"
-					icon8={<DiPostgresql />}
-					icon8Color="hsla(204, 100%, 32%, 1)"
-					icon8Alt="PostgreSQL"
-					icon9={<DiGit />}
-					icon9Color="hsla(10, 87%, 56%, 1)"
-					icon9Alt="Git"
-					icon10={<DiPhotoshop />}
-					icon10Color="hsla(210, 95%, 77%, 1)"
-					icon10Alt="Photoshop"
-					icon11={<SiFigma />}
-					icon11Color="hsla(148, 100%, 38%, 1)"
-					icon11Alt="Figma"
-					icon12={<DiVisualstudio />}
-					icon12Color="hsla(207, 100%, 42%, 1)"
-					icon12Alt="VSCode"
-					icon13={<DiPhp />}
-					icon13Color="hsla(229, 29%, 64%, 1)"
-					icon13Alt="PHP"
-					icon14={<SiWebpack />}
-					icon14Color="hsla(206, 75%, 43%, 1)"
-					icon14Alt="Webpack"
-					icon15={<SiWoocommerce />}
-					icon15Color="hsla(312, 26%, 47%, 1)"
-					icon15Alt="WooCommerce"
-					icon16={<SiRaspberrypi />}
-					icon16Color="hsla(343, 77%, 44%, 1)"
-					icon16Alt="RaspberryPi"
-				/>
+				<InfoImage icons={iconList} />
 			</div>
 		</div>
 	)
--- src/components/infoimage/InfoImage.js
+++ src/components/infoimage/InfoImage.js
@@ -3,179 +3,29 @@ import "../../styles/global.css"
 import styles from "./infoimage.module.css"
 
 export default function Hero(props) {
-	const {
-		icon1,
-		icon2,
-		icon3,
-		icon4,
-		icon5,
-		icon6,
-		icon7,
-		icon8,
-		icon9,
-		icon10,
-		icon11,
-		icon12,
-		icon13,
-		icon14,
-		icon15,
-		icon16,
-	} = props
-	const {
-		icon1Color,
-		icon2Color,
-		icon3Color,
-		icon4Color,
-		icon5Color,
-		icon6Color,
-		icon7Color,
-		icon8Color,
-		icon9Color,
-		icon10Color,
-		icon11Color,
-		icon12Color,
-		icon13Color,
-		icon14Color,
-		icon15Color,
-		icon16Color,
-	} = props
+	function IconElement(iconObj) {
+		return (
+			<div
+				className={styles.icon}
+				style={{ color: `${iconObj.color}` }}
+				title={iconObj.alt}
+				id={iconObj.alt.replace(/\s/g, "-")}
+			>
+				{iconObj.icon}
+			</div>
+		)
+	}
 
-	const {
-		icon1Alt,
-		icon2Alt,
-		icon3Alt,
-		icon4Alt,
-		icon5Alt,
-		icon6Alt,
-		icon7Alt,
-		icon8Alt,
-		icon9Alt,
-		icon10Alt,
-		icon11Alt,
-		icon12Alt,
-		icon13Alt,
-		icon14Alt,
-		icon15Alt,
-		icon16Alt,
-	} = props
+	const iconList = props.icons.map(icon => {
+		return IconElement(icon)
+	})
+
+	console.log(`console-icons= ${iconList}`)
 
 	return (
 		<div className={styles.infoImageContainer}>
 			<div className={styles.background}>
-				<div className={styles.iconContainer}>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon1Color}` }}
-						title={icon1Alt}
-					>
-						{icon1}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon2Color}` }}
-						title={icon2Alt}
-					>
-						{icon2}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon3Color}` }}
-						title={icon3Alt}
-					>
-						{icon3}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon4Color}` }}
-						title={icon4Alt}
-					>
-						{icon4}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon5Color}` }}
-						title={icon5Alt}
-					>
-						{icon5}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon6Color}` }}
-						title={icon6Alt}
-					>
-						{icon6}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon7Color}` }}
-						title={icon7Alt}
-					>
-						{icon7}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon8Color}` }}
-						title={icon8Alt}
-					>
-						{icon8}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon9Color}` }}
-						title={icon9Alt}
-					>
-						{icon9}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon10Color}` }}
-						title={icon10Alt}
-					>
-						{icon10}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon11Color}` }}
-						title={icon11Alt}
-					>
-						{icon11}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon12Color}` }}
-						title={icon12Alt}
-					>
-						{icon12}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon13Color}` }}
-						title={icon13Alt}
-					>
-						{icon13}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon14Color}` }}
-						title={icon14Alt}
-					>
-						{icon14}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon15Color}` }}
-						title={icon15Alt}
-					>
-						{icon15}
-					</div>
-					<div
-						className={styles.icon}
-						style={{ color: `${icon16Color}` }}
-						title={icon16Alt}
-					>
-						{icon16}
-					</div>
-				</div>
+				<div className={styles.iconContainer}>{iconList}</div>
 			</div>
 		</div>
 	)
