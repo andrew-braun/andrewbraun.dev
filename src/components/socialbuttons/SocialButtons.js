ebc17c964c5e4906ddc4b3807609b30eac631f46
Andrew Braun <andrew@andrewbraun.dev>
2021-02-06 17:25:42 +0400

    fix: Dynamically create class names for social button

--- src/components/socialbuttons/SocialButtons.js
+++ src/components/socialbuttons/SocialButtons.js
@@ -77,9 +77,10 @@ export default function SocialButtons() {
 
 	const socialArray = Object.values(data.allSocialInfoJson.edges[0].node)
 	const socialElementList = socialArray.map(item => {
-		console.log(item.name)
 		const Icon = components[`${item.name}Icon`]
-		console.log(components[`${item.name}Icon`])
+		const iconClassName =
+			styles[`${item.name[0].toLowerCase() + item.name.slice(1)}Icon`]
+
 		return (
 			<div className={styles.socialButton} key={`${item.name}-button-key`}>
 				<a
@@ -88,7 +89,7 @@ export default function SocialButtons() {
 					alt={item.name}
 					className={styles.socialLink}
 				>
-					<span className={`styles.${item.name}Icon`}>
+					<span className={iconClassName}>
 						<Icon />
 					</span>
 					<span class={styles.socialButtonLabel}>{item.name}</span>
