d759c98230243ca14b026f95d8c68af5b248013d
Andrew Braun <andrew@andrewbraun.dev>
2021-09-14 14:35:16 -0400

    feat: Layout styles

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<header class="layout-header">
+	AndrewBraun.dev
+	<nav>
+		<a href="/">Home</a>
+	</nav>
+</header>
+<main class="layout-main">
+	<slot />
+</main>
+<footer class="layout-footer">
+	<p>Made by Andrew Braun</p>
+</footer>
+
+<style>
+	.layout-main {
+		min-height: 80vh;
+	}
+	.layout-footer {
+		display: flex;
+		justify-content: center;
+	}
+</style>
