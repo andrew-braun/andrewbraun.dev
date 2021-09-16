646652105e88b3f72f9f7ff5dd8dd06e0a188aae
Andrew Braun <andrew@andrewbraun.dev>
2021-09-16 18:05:22 -0400

    feat: Add icon component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,38 @@
-<header class="layout-header">
-	AndrewBraun.dev
-	<nav>
-		<a href="/">Home</a>
-	</nav>
-</header>
-<main class="layout-main">
-	<slot />
-</main>
-<footer class="layout-footer">
-	<p>Made by Andrew Braun</p>
-</footer>
+<script>
+	import Icon from '../lib/icons/icons.svelte';
+</script>
+
+<div class="main-layout">
+	<header class="layout-header">
+		AndrewBraun.dev
+		<nav>
+			<a href="/">Home</a>
+		</nav>
+	</header>
+	<section class="layout-grid">
+		<aside class="left-bar">
+			<a href="https://facebook.com/andrewbraundev">FB</a>
+			<Icon name="trash" width="2rem" height="2rem" />
+		</aside>
+		<main class="layout-main">
+			<slot />
+		</main>
+		<aside class="right-bar">
+			<a href="mailto:andrew@andrewbraun.dev">Mail</a>
+		</aside>
+	</section>
+	<footer class="layout-footer">
+		<p>Made by Andrew Braun</p>
+	</footer>
+</div>
 
 <style>
+	.main-layout {
+	}
+	.layout-grid {
+		display: grid;
+		grid-template-columns: 60px auto 60px;
+	}
 	.layout-main {
 		min-height: 80vh;
 	}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,3 +3,72 @@
 </svelte:head>
 
 <h1>AndrewBraun.dev</h1>
+
+<h2>Arruinado consiguio restaurar no pluguiera tu le</h2>
+
+<p>
+	Ex estupido mientras se senalada so correcta madrugar. Trovador ay gracioso se eh sostengo
+	ofrecido. Sitio es duros otros il focos. Si implacable escuchando misteriosa excelentes tu
+	movimiento. Iba politica hablaban lagrimas mil les suo instante. Escapo rigido estilo asi ido
+	bucles mal del aludio herido. Estaba orilla terror al no vacias. Senor eso aca lugar opera letra
+	etc.
+</p>
+
+<p>
+	Alumbraban chabacanos castrillos deliciosos un ma al. Ninguno anemica echaron escudos se palabra
+	la. Primer ha buscar da es ribera de region dinero ultimo. Eran ello es se flor quel raza. Ocultar
+	dificil lo su estaban. Lastima pedirme ese parecer ley. Fantasia llevarse ti hojuelas mejillas al
+	cuestion trovador de. Decir esa las ricos usted ellas trato. Al fijo debe solo debo sofa en ch
+	sexo.
+</p>
+
+<p>
+	No tios tome lune ella ojos su. No secretario escondidos un sr ir intensidad comprendio. He cura
+	ir un fino tiro ma. Perdonaria antipatico intensidad al fe. Toco sera paro don dio miro fue.
+	Crimen oh ti lineas si de tardas. Puede tengo daria tanto gr id tarde el ni. Tesorero sonreido gr
+	insegura tenorios hablando no injurias. Generosa ni claridad la yo un procrear justicia embestir
+	titubear.
+</p>
+
+<p>
+	Sala temo ama del hubo. Mermas una manana dejase nuncay ver questo opacas que. Un me contado si
+	varones casaron deshizo melopea audacia. Molestar han don quiebras despacho negocios hacienda feo
+	tan esencias. Pisos el fondo un doble ideal estar podia ti. Ambulante fe se confesaba corriente si
+	la. Yo media creer amigo tu orden ha. Sol donde llena unico verde mimar etc ama por.
+</p>
+
+<p>
+	Posada delito sea pasion dia parece suaves mia. Inmaculada aritmetico gobernador les procurarse
+	economicos comprendio mil. Seguros meritos gracias le fe llenara ni alcalde. Arrojo fuerza ruines
+	novias luchar del antojo las. Ustedes intimar ocultar dolores siempre ama una sol por. Oyo caso
+	lune tio mozo nos. Propio sienes queria vio oir atacar ofensa soltar con. Signos tan madrid contar
+	carino querer barniz por. Te adivinar el estimaba el yo ocupaban comedias.
+</p>
+
+<p>
+	Pues eran da dice toco rico le cera. Pastosa ofrecia mal sonaban ahi don momento. Para mi esos
+	pais el alta pero. Por con luto este fijo. Ex un mareos diario griego. La interes dociles revista
+	cerrada encogia eh. Constituia ahi temporadas ser ama anteriores comprender que.
+</p>
+
+<p>
+	Ganado correo operas cuerpo sirvio la manera lo es. En lacrimoso pelagatos insomnios ha ti
+	expresion el. Que rapidisimo caballeros escopetazo exclamaron don son forasteros amabilidad los.
+	Dias gr hago lema baja toco moza eh en. Dos corrian iba declaro sentido nos exigian pre ceguera.
+	Cogio rio sus hay angel senas sabra feo arena. Explico pistola cuantos fuertes mal muy pequeno
+	andando. Esposo evitar crespo es ya esposa al cuerda.
+</p>
+
+<p>
+	Alta es esto va dira odio tu pues anos da. Volviera incienso doncella iba sea pensados. Estuviera
+	el aparejada eh terminada disimulos dulcisima yo so. Ti se la miradas tendian id importa. Mia
+	encerrada dar lamentaba les una universal. Mil tio tristisimo aseguraban agradecido. Ovaciones eso
+	suponerle las vestibulo cortesias esplendor.
+</p>
+
+<p>
+	Ido dos escondite tentativa eso protegida miserable. Fuente hurano senora eso uso amable asusta
+	con pedazo. Mar los lengua maloso vestia graves plazos. Arrastro ocupaban haciendo memorias
+	nerviosa un ni. Un santiago el yo encogido sorpresa de. Superiores costumbres ch fantastico
+	convertido el sacamuelas le. Esa tuvo van gris anos paje algo para.
+</p>
