bc0c656d546a3709d53cda768db4e6981ff78cc6
Andrew Braun <andrew@andrewbraun.dev>
2021-09-18 15:32:00 -0400

    feat: Add icon SVGs

--- src/lib/icons/icons.svelte
+++ src/lib/icons/icons.svelte
@@ -1,18 +1,43 @@
 <script>
 	export let name;
-	export let width = '1rem';
-	export let height = '1rem';
+	export let width = "2rem";
+	export let height = "2rem";
+	export let strokeColor = "black";
+	export let fillColor = "none";
+	export let strokeWidth = 2;
 	export let focusable = false;
+	export let strokeLinecap = "round";
+	export let strokeLinejoin = "round";
+	export let viewBox = "0 0 24 24";
+
 	let icons = {
-		save: {
-			box: 24,
-			name: 'save',
-			svg: `<g stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/><path d="M17 21v-8H7v8"/><path d="M7 3v5h8"/></g>`
+		facebook: {
+			name: "facebook",
+			svg: `<path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>`
 		},
-		trash: {
-			box: 32,
-			name: 'trash',
-			svg: `<path d="M12 12h2v12h-2z" /><path d="M18 12h2v12h-2z" /><path d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20z" /><path d="M12 2h8v2h-8z" />`
+		twitter: {
+			name: "twitter",
+			svg: `<path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>`
+		},
+		codepen: {
+			name: "codepen",
+			svg: `<polygon points="12 2 22 8.5 22 15.5 12 22 2 15.5 2 8.5 12 2"></polygon><line x1="12" y1="22" x2="12" y2="15.5"></line><polyline points="22 8.5 12 15.5 2 8.5"></polyline><polyline points="2 15.5 12 8.5 22 15.5"></polyline><line x1="12" y1="2" x2="12" y2="8.5"></line>`
+		},
+		email: {
+			name: "email",
+			svg: `<path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline>`
+		},
+		linkedin: {
+			name: "linkedin",
+			svg: `<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle>`
+		},
+		github: {
+			name: "github",
+			svg: `<path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>`
+		},
+		polywork: {
+			name: "polywork",
+			svg: `<path d="M19.125 0H4.875A4.865 4.865 0 0 0 0 4.875v14.25C0 21.825 2.175 24 4.875 24h6.6c2.7 0 4.875-2.175 4.875-4.875V16.65h2.775c2.7 0 4.875-2.175 4.875-4.875v-6.9C24 2.175 21.825 0 19.125 0zM16.5 1.275h2.625a3.6 3.6 0 0 1 3.6 3.6v2.7H16.5v-6.3zM15.075 9v6.45H8.85V9h6.225zM8.85 1.2h6.225v6.375H8.85V1.2zM1.275 4.8a3.6 3.6 0 0 1 3.6-3.6H7.5v6.375H1.275V4.8zM7.5 9v6.45H1.2V9h6.3zm0 13.725H4.8a3.6 3.6 0 0 1-3.6-3.6V16.8h6.3v5.925zm7.575-3.525a3.6 3.6 0 0 1-3.6 3.6H8.85v-5.925h6.225V19.2zm7.65-7.35a3.6 3.6 0 0 1-3.6 3.6H16.5V9h6.225v2.85z"/>`
 		}
 	};
 	let displayIcon = icons[name];
@@ -23,5 +48,10 @@
 	{focusable}
 	{width}
 	{height}
-	viewBox="0 0 {displayIcon.box} {displayIcon.box}">{@html displayIcon.svg}</svg
+	stroke={strokeColor}
+	fill={fillColor}
+	stroke-width={strokeWidth}
+	stroke-linejoin={strokeLinejoin}
+	stroke-linecap={strokeLinecap}
+	{viewBox}>{@html displayIcon.svg}</svg
 >
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Icon from '../lib/icons/icons.svelte';
+	import Icon from "../lib/icons/icons.svelte";
 </script>
 
 <div class="main-layout">
@@ -11,14 +11,21 @@
 	</header>
 	<section class="layout-grid">
 		<aside class="left-bar">
-			<a href="https://facebook.com/andrewbraundev">FB</a>
-			<Icon name="trash" width="2rem" height="2rem" />
+			<a href="https://linkedin.com/in/andrewwestonbraun"> <Icon name="linkedin" /></a>
+			<a href="https://github.com/andrew-braun"> <Icon name="github" /></a>
+			<a href="https://twitter.com/braunecon"><Icon name="twitter" /></a>
+			<a href="https://codepen.com/andrewbraun"> <Icon name="codepen" /></a>
+			<a href="https://facebook.com/andrewbraun.dev"> <Icon name="facebook" /></a>
+			<a href="https://polywork.com/andrewbraun" style="padding:0.2rem">
+				<Icon name="polywork" strokeWidth="1.3" width="1.7rem" height="1.7rem" /></a
+			>
+			<a href="mailto:andrew@andrewbraun.dev"><Icon name="email" /></a>
 		</aside>
 		<main class="layout-main">
 			<slot />
 		</main>
 		<aside class="right-bar">
-			<a href="mailto:andrew@andrewbraun.dev">Mail</a>
+			<a href="mailto:andrew@andrewbraun.dev"><Icon name="email" /></a>
 		</aside>
 	</section>
 	<footer class="layout-footer">
