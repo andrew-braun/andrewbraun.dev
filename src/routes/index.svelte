e5243e9b32b3c679b94e165b274423c9e33fe4f9
Andrew Braun <andrew@andrewbraun.dev>
2021-10-18 22:46:44 +0400

    feat: Create front page sections

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,88 +6,109 @@
 	<title>AndrewBraun.dev - WordPress, React, Svelte, and More</title>
 </svelte:head>
 
-<section role="document" class="content-container">
-	<h1>AndrewBraun.dev</h1>
+<div class="content-container">
+	<section class="hero-section">
+		<h1>AndrewBraun.dev</h1>
+	</section>
+	<section class="my-work-section page-section">
+		<h2 id="my-work">My Work</h2>
+		<p>
+			No tios tome lune ella ojos su. No secretario escondidos un sr ir intensidad comprendio. He
+			cura ir un fino tiro ma. Perdonaria antipatico intensidad al fe. Toco sera paro don dio miro
+			fue. Crimen oh ti lineas si de tardas. Puede tengo daria tanto gr id tarde el ni. Tesorero
+			sonreido gr insegura tenorios hablando no injurias. Generosa ni claridad la yo un procrear
+			justicia embestir titubear.
+		</p>
 
-	<h2 id="about">About</h2>
+		<p>
+			Sala temo ama del hubo. Mermas una manana dejase nuncay ver questo opacas que. Un me contado
+			si varones casaron deshizo melopea audacia. Molestar han don quiebras despacho negocios
+			hacienda feo tan esencias. Pisos el fondo un doble ideal estar podia ti. Ambulante fe se
+			confesaba corriente si la. Yo media creer amigo tu orden ha. Sol donde llena unico verde mimar
+			etc ama por.
+		</p>
 
-	<p>
-		Ex estupido mientras se senalada so correcta madrugar. Trovador ay gracioso se eh sostengo
-		ofrecido. Sitio es duros otros il focos. Si implacable escuchando misteriosa excelentes tu
-		movimiento. Iba politica hablaban lagrimas mil les suo instante. Escapo rigido estilo asi ido
-		bucles mal del aludio herido. Estaba orilla terror al no vacias. Senor eso aca lugar opera letra
-		etc.
-	</p>
+		<p>
+			Posada delito sea pasion dia parece suaves mia. Inmaculada aritmetico gobernador les
+			procurarse economicos comprendio mil. Seguros meritos gracias le fe llenara ni alcalde. Arrojo
+			fuerza ruines novias luchar del antojo las. Ustedes intimar ocultar dolores siempre ama una
+			sol por. Oyo caso lune tio mozo nos. Propio sienes queria vio oir atacar ofensa soltar con.
+			Signos tan madrid contar carino querer barniz por. Te adivinar el estimaba el yo ocupaban
+			comedias.
+		</p>
 
-	<p>
-		Alumbraban chabacanos castrillos deliciosos un ma al. Ninguno anemica echaron escudos se palabra
-		la. Primer ha buscar da es ribera de region dinero ultimo. Eran ello es se flor quel raza.
-		Ocultar dificil lo su estaban. Lastima pedirme ese parecer ley. Fantasia llevarse ti hojuelas
-		mejillas al cuestion trovador de. Decir esa las ricos usted ellas trato. Al fijo debe solo debo
-		sofa en ch sexo.
-	</p>
+		<p>
+			Pues eran da dice toco rico le cera. Pastosa ofrecia mal sonaban ahi don momento. Para mi esos
+			pais el alta pero. Por con luto este fijo. Ex un mareos diario griego. La interes dociles
+			revista cerrada encogia eh. Constituia ahi temporadas ser ama anteriores comprender que.
+		</p>
+	</section>
+	<section class="about-section page-section">
+		<h2 id="about">About</h2>
 
-	<h2 id="my-work">My Work</h2>
-	<p>
-		No tios tome lune ella ojos su. No secretario escondidos un sr ir intensidad comprendio. He cura
-		ir un fino tiro ma. Perdonaria antipatico intensidad al fe. Toco sera paro don dio miro fue.
-		Crimen oh ti lineas si de tardas. Puede tengo daria tanto gr id tarde el ni. Tesorero sonreido
-		gr insegura tenorios hablando no injurias. Generosa ni claridad la yo un procrear justicia
-		embestir titubear.
-	</p>
+		<p>
+			Ex estupido mientras se senalada so correcta madrugar. Trovador ay gracioso se eh sostengo
+			ofrecido. Sitio es duros otros il focos. Si implacable escuchando misteriosa excelentes tu
+			movimiento. Iba politica hablaban lagrimas mil les suo instante. Escapo rigido estilo asi ido
+			bucles mal del aludio herido. Estaba orilla terror al no vacias. Senor eso aca lugar opera
+			letra etc.
+		</p>
 
-	<p>
-		Sala temo ama del hubo. Mermas una manana dejase nuncay ver questo opacas que. Un me contado si
-		varones casaron deshizo melopea audacia. Molestar han don quiebras despacho negocios hacienda
-		feo tan esencias. Pisos el fondo un doble ideal estar podia ti. Ambulante fe se confesaba
-		corriente si la. Yo media creer amigo tu orden ha. Sol donde llena unico verde mimar etc ama
-		por.
-	</p>
+		<p>
+			Alumbraban chabacanos castrillos deliciosos un ma al. Ninguno anemica echaron escudos se
+			palabra la. Primer ha buscar da es ribera de region dinero ultimo. Eran ello es se flor quel
+			raza. Ocultar dificil lo su estaban. Lastima pedirme ese parecer ley. Fantasia llevarse ti
+			hojuelas mejillas al cuestion trovador de. Decir esa las ricos usted ellas trato. Al fijo debe
+			solo debo sofa en ch sexo.
+		</p>
+	</section>
+	<section class="contact-section page-section">
+		<h2 id="contact">Contact</h2>
+		<p>
+			Ganado correo operas cuerpo sirvio la manera lo es. En lacrimoso pelagatos insomnios ha ti
+			expresion el. Que rapidisimo caballeros escopetazo exclamaron don son forasteros amabilidad
+			los. Dias gr hago lema baja toco moza eh en. Dos corrian iba declaro sentido nos exigian pre
+			ceguera. Cogio rio sus hay angel senas sabra feo arena. Explico pistola cuantos fuertes mal
+			muy pequeno andando. Esposo evitar crespo es ya esposa al cuerda.
+		</p>
 
-	<p>
-		Posada delito sea pasion dia parece suaves mia. Inmaculada aritmetico gobernador les procurarse
-		economicos comprendio mil. Seguros meritos gracias le fe llenara ni alcalde. Arrojo fuerza
-		ruines novias luchar del antojo las. Ustedes intimar ocultar dolores siempre ama una sol por.
-		Oyo caso lune tio mozo nos. Propio sienes queria vio oir atacar ofensa soltar con. Signos tan
-		madrid contar carino querer barniz por. Te adivinar el estimaba el yo ocupaban comedias.
-	</p>
+		<p>
+			Alta es esto va dira odio tu pues anos da. Volviera incienso doncella iba sea pensados.
+			Estuviera el aparejada eh terminada disimulos dulcisima yo so. Ti se la miradas tendian id
+			importa. Mia encerrada dar lamentaba les una universal. Mil tio tristisimo aseguraban
+			agradecido. Ovaciones eso suponerle las vestibulo cortesias esplendor.
+		</p>
 
-	<p>
-		Pues eran da dice toco rico le cera. Pastosa ofrecia mal sonaban ahi don momento. Para mi esos
-		pais el alta pero. Por con luto este fijo. Ex un mareos diario griego. La interes dociles
-		revista cerrada encogia eh. Constituia ahi temporadas ser ama anteriores comprender que.
-	</p>
-	<h2 id="contact">Contact</h2>
-	<p>
-		Ganado correo operas cuerpo sirvio la manera lo es. En lacrimoso pelagatos insomnios ha ti
-		expresion el. Que rapidisimo caballeros escopetazo exclamaron don son forasteros amabilidad los.
-		Dias gr hago lema baja toco moza eh en. Dos corrian iba declaro sentido nos exigian pre ceguera.
-		Cogio rio sus hay angel senas sabra feo arena. Explico pistola cuantos fuertes mal muy pequeno
-		andando. Esposo evitar crespo es ya esposa al cuerda.
-	</p>
-
-	<p>
-		Alta es esto va dira odio tu pues anos da. Volviera incienso doncella iba sea pensados.
-		Estuviera el aparejada eh terminada disimulos dulcisima yo so. Ti se la miradas tendian id
-		importa. Mia encerrada dar lamentaba les una universal. Mil tio tristisimo aseguraban
-		agradecido. Ovaciones eso suponerle las vestibulo cortesias esplendor.
-	</p>
-
-	<p>
-		Ido dos escondite tentativa eso protegida miserable. Fuente hurano senora eso uso amable asusta
-		con pedazo. Mar los lengua maloso vestia graves plazos. Arrastro ocupaban haciendo memorias
-		nerviosa un ni. Un santiago el yo encogido sorpresa de. Superiores costumbres ch fantastico
-		convertido el sacamuelas le. Esa tuvo van gris anos paje algo para.
-	</p>
-</section>
+		<p>
+			Ido dos escondite tentativa eso protegida miserable. Fuente hurano senora eso uso amable
+			asusta con pedazo. Mar los lengua maloso vestia graves plazos. Arrastro ocupaban haciendo
+			memorias nerviosa un ni. Un santiago el yo encogido sorpresa de. Superiores costumbres ch
+			fantastico convertido el sacamuelas le. Esa tuvo van gris anos paje algo para.
+		</p>
+	</section>
+</div>
 
 <style>
 	.content-container {
-		padding: 7rem calc(1.5rem + 4vw) 0 calc(2.5rem + 4vw);
+		/* padding: 7rem calc(1.5rem + 4vw) 0 calc(2.5rem + 4vw); */
 	}
-	@media only screen and (max-width: 480px) {
-		.content-container {
-			padding: 7rem calc(1.5rem + 4vw);
-		}
+
+	.page-section {
+		padding: 1rem calc(1.5rem + 4vw) 1rem calc(2.5rem + 4vw);
+	}
+	.hero-section {
+		width: 100%;
+		padding: 7rem calc(1.5rem + 4vw) 0 calc(2.5rem + 4vw);
+		min-height: 80vh;
+		background: lightblue;
+	}
+	.my-work-section {
+		background: lightgreen;
+	}
+	.contact-section {
+		background: lightslategrey;
+	}
+	.about-section {
+		background: skyblue;
 	}
 </style>
