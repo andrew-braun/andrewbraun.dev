9b7e3bf0d0abbc30ff547945699840894a7765a5
Andrew Braun <andrew@andrewbraun.dev>
2021-11-05 19:28:11 +0400

    build: Attempt new Netlify build

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
-import adapter from "@sveltejs/adapter-netlify";
+// import adapter from "@sveltejs/adapter-netlify";
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		adapter: adapter(),
+// 		target: "#svelte"
+// 	}
+// };
+
+// export default config;
 
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from "@sveltejs/adapter-netlify";
+
 const config = {
+	extensions: [".svelte", ".md", ".svelte.md"],
+
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
 		target: "#svelte"
 	}
 };
