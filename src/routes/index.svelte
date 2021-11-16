376d9f868aab3de024b3f131b9af0deee45e9b1b
Andrew Braun <andrew@andrewbraun.dev>
2021-11-16 12:54:28 +0400

    chore: Update npm packages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,8 +13,6 @@
 	});
 </script>
 
-<svelte:head />
-
 <div class="content-container">
 	<section class="hero-section">
 		<div class="hero-flavor-container">
@@ -213,41 +211,11 @@
 			{/if}
 		</div>
 	</section>
-	<!---
 	<section class="my-work-section page-section">
 		<h2 id="my-work">My Work</h2>
-		<p>
-			No tios tome lune ella ojos su. No secretario escondidos un sr ir intensidad comprendio. He
-			cura ir un fino tiro ma. Perdonaria antipatico intensidad al fe. Toco sera paro don dio miro
-			fue. Crimen oh ti lineas si de tardas. Puede tengo daria tanto gr id tarde el ni. Tesorero
-			sonreido gr insegura tenorios hablando no injurias. Generosa ni claridad la yo un procrear
-			justicia embestir titubear.
-		</p>
-
-		<p>
-			Sala temo ama del hubo. Mermas una manana dejase nuncay ver questo opacas que. Un me contado
-			si varones casaron deshizo melopea audacia. Molestar han don quiebras despacho negocios
-			hacienda feo tan esencias. Pisos el fondo un doble ideal estar podia ti. Ambulante fe se
-			confesaba corriente si la. Yo media creer amigo tu orden ha. Sol donde llena unico verde mimar
-			etc ama por.
-		</p>
-
-		<p>
-			Posada delito sea pasion dia parece suaves mia. Inmaculada aritmetico gobernador les
-			procurarse economicos comprendio mil. Seguros meritos gracias le fe llenara ni alcalde. Arrojo
-			fuerza ruines novias luchar del antojo las. Ustedes intimar ocultar dolores siempre ama una
-			sol por. Oyo caso lune tio mozo nos. Propio sienes queria vio oir atacar ofensa soltar con.
-			Signos tan madrid contar carino querer barniz por. Te adivinar el estimaba el yo ocupaban
-			comedias.
-		</p>
-
-		<p>
-			Pues eran da dice toco rico le cera. Pastosa ofrecia mal sonaban ahi don momento. Para mi esos
-			pais el alta pero. Por con luto este fijo. Ex un mareos diario griego. La interes dociles
-			revista cerrada encogia eh. Constituia ahi temporadas ser ama anteriores comprender que.
-		</p>
 	</section>
-	<section class="about-section page-section">
+
+	<!-- <section class="about-section page-section">
 		<h2 id="about">About</h2>
 
 		<p>
@@ -265,8 +233,8 @@
 			hojuelas mejillas al cuestion trovador de. Decir esa las ricos usted ellas trato. Al fijo debe
 			solo debo sofa en ch sexo.
 		</p>
-	</section>
-	<section class="contact-section page-section">
+	</section> -->
+	<!-- <section class="contact-section page-section">
 		<h2 id="contact">Contact</h2>
 		<p>
 			Ganado correo operas cuerpo sirvio la manera lo es. En lacrimoso pelagatos insomnios ha ti
@@ -289,8 +257,7 @@
 			memorias nerviosa un ni. Un santiago el yo encogido sorpresa de. Superiores costumbres ch
 			fantastico convertido el sacamuelas le. Esa tuvo van gris anos paje algo para.
 		</p>
-	</section>
--->
+	</section> -->
 </div>
 
 <style>
