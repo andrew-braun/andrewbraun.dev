76c80b44d1fd535ff782cad55171e66583b1396f
Andrew Braun <andrew@andrewbraun.dev>
2021-12-12 11:54:23 +0400

    feat: Sort portfolio cards by order listed in CMS

--- src/lib/portfolio/PortfolioContainer.svelte
+++ src/lib/portfolio/PortfolioContainer.svelte
@@ -2,11 +2,14 @@
 	import ProjectCard from "./ProjectCard.svelte";
 
 	export let projects;
+	const sortedProjects = projects.sort(
+		(projectA, projectB) => projectA.order_priority > projectB.order_priority
+	);
 </script>
 
 <div class="portfolio-container">
 	{#if projects}
-		{#each projects as project}
+		{#each sortedProjects as project}
 			<ProjectCard
 				name={project.name}
 				featuredImageUrl={project.featured_image.formats}
