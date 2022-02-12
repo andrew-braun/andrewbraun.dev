c7867e0cceaad8b113e1babfcef004a95a0c90f5
Andrew Braun <andrew@andrewbraun.dev>
2022-02-12 15:24:22 +0400

    feat: Added tabs component

---
+++ src/lib/ui/tabbed-content/Tabs.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+	export const tabsKey = {};
+</script>
+
+<script>
+	import { setContext, onMount } from "svelte";
+	import { writable } from "svelte/store";
+
+	const tabs = writable([]);
+	const selectedTab = writable(null);
+	setContext(tabsKey, {
+		tabs,
+		selectedTab
+	});
+
+	onMount(() => {
+		if ($tabs.length > 0) $selectedTab = $tabs[0];
+	});
+</script>
+
+<div class="tabs-container">
+	<div class="tabs">
+		<slot isTitle={true} isContent={false} />
+	</div>
+	<div class="tab-content">
+		<slot isTitle={false} isContent={true} />
+	</div>
+</div>
