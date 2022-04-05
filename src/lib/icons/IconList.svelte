864585db17f1aed55fc85b8dc0f9fee1c23312a0
Andrew Braun <andrew@andrewbraun.dev>
2022-04-06 00:03:18 +0400

    feat: Create icon list component system

---
+++ src/lib/icons/IconList.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let title;
+</script>
+
+<div class="icon-list-container">
+	<h3 class="icon-list-title">{title}</h3>
+	<div class="icon-list">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.icon-list-container {
+		max-width: 90%;
+		padding: 0.5rem 1rem;
+	}
+	.icon-list-title {
+		margin-bottom: 0.5rem;
+	}
+	.icon-list {
+		display: flex;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+		max-width: 80%;
+		margin: auto;
+	}
+</style>
---
+++ src/lib/icons/IconifyIcon.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Icon from "@iconify/svelte";
+
+	export let name;
+	export let subtitle;
+</script>
+
+<div class="icon-container">
+	<Icon icon={name} alt={subtitle} />
+	<p class="icon-subtitle">{subtitle}</p>
+</div>
+
+<style>
+	.icon-container {
+		text-align: center;
+	}
+	.icon-list-icon {
+		display: flex;
+		flex-direction: column;
+	}
+	.icon-subtitle {
+		margin: 0;
+	}
+	:global(.iconify--vscode-icons) {
+		position: relative;
+		width: 50px;
+		height: 50px;
+		margin: 0.25rem 0.25rem 0 0.25rem;
+	}
+</style>
