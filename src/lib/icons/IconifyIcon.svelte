c2bd5c352cb145936ea737c03dac3a469fc5eef2
Andrew Braun <andrew@andrewbraun.dev>
2022-04-07 23:32:13 +0400

    content: Add more icons

--- src/lib/icons/IconifyIcon.svelte
+++ src/lib/icons/IconifyIcon.svelte
@@ -3,28 +3,47 @@
 
 	export let name;
 	export let subtitle;
+	export let background;
 </script>
 
-<div class="icon-container">
-	<Icon icon={name} alt={subtitle} />
+<div class={`icon-container`}>
+	<div class={`icon ${background ? background : ""}`}>
+		<Icon icon={name} alt={subtitle} />
+	</div>
 	<p class="icon-subtitle">{subtitle}</p>
 </div>
 
 <style>
 	.icon-container {
-		text-align: center;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 	.icon-list-icon {
 		display: flex;
 		flex-direction: column;
 	}
+
+	.icon {
+		display: flex;
+		justify-content: center;
+		width: 60px;
+		height: 60px;
+		padding: 0.25rem;
+		text-align: center;
+	}
 	.icon-subtitle {
 		margin: 0;
 	}
-	:global(.iconify--vscode-icons) {
+
+	:global(svg.iconify) {
 		position: relative;
 		width: 50px;
 		height: 50px;
-		margin: 0.25rem 0.25rem 0 0.25rem;
+		/* margin: 0.25rem 0.25rem 0 0.25rem; */
+	}
+	.white-background-circle {
+		background: white;
+		border-radius: 50%;
 	}
 </style>
