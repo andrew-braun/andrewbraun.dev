79a51036a5bed2ae772edacdc1c304ec6abad578
Andrew Braun <andrew@andrewbraun.dev>
2022-04-19 22:19:44 +0400

    feat: Add services page

---
+++ src/routes/services.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div id="services-page-container" class="global-top-section">
+	<h1>Services</h1>
+</div>
+
+<style>
+	#services-page-container {
+		padding-left: 10%;
+		padding-right: 10%;
+	}
+</style>
